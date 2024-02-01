240b56e6d20a5de2054b19628b815e8429c1d902
Matt <emailnageografie@gmail.com>
2024-02-01 12:21:35 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module org.example.tline {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens org.example.tline to javafx.fxml;
+    exports org.example.tline;
+}
