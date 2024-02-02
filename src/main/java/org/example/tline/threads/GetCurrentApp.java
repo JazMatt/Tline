30523c51cb9ee61785a06ab345bc8e8cea492006
JazMatt <mat.jazdzewski@outlook.com>
2024-02-02 14:50:02 +0100

    Implemented screen time tracking and reading from database

--- src/main/java/org/example/tline/threads/GetCurrentApp.java
+++ src/main/java/org/example/tline/threads/GetCurrentApp.java
@@ -4,47 +4,68 @@ import com.sun.jna.Native;
 import com.sun.jna.platform.win32.*;
 import com.sun.jna.ptr.IntByReference;
 
+import java.util.TreeMap;
+
 public class GetCurrentApp implements Runnable {
 
     private UpdateDataBase updateDB;
+    private TreeMap<String, String[]> usageData;
     @Override
     public void run() {
 
         updateDB = new UpdateDataBase();
+        usageData = updateDB.getUsageData();
+
+        System.out.println(usageData);
+        int secs = 0;
 
         while (true) {
+
             try {
                 Thread.sleep(1000);
+                secs ++;
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-            WinDef.HWND hwnd = User32.INSTANCE.GetForegroundWindow();
 
-            // GET PATH OF ACTIVE APP
+            // Get path of active app
+            WinDef.HWND hwnd = User32.INSTANCE.GetForegroundWindow();
             IntByReference processId = new IntByReference();
             User32.INSTANCE.GetWindowThreadProcessId(hwnd, processId);
             WinNT.HANDLE processHandle = Kernel32.INSTANCE.OpenProcess(
                     WinNT.PROCESS_QUERY_INFORMATION | WinNT.PROCESS_VM_READ, false, processId.getValue());
-
             char[] exePath = new char[1024];
             Psapi.INSTANCE.GetModuleFileNameExW(processHandle, null, exePath, exePath.length);
             Kernel32.INSTANCE.CloseHandle(processHandle);
-
             String exePathStr = Native.toString(exePath);
-//        System.out.println("Path: " + exePathStr);  // "C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2023.3.2\bin\idea64.exe"
+//          System.out.println("Path: " + exePathStr);  // "C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2023.3.2\bin\idea64.exe"
 
-            // FORMAT IT
-            String activeAppNameNotFormatted = exePathStr.substring(exePathStr.lastIndexOf("\\") + 1);
-            String activeAppName = activeAppNameNotFormatted.replace(".exe", "").toLowerCase();
-            char firstChar = Character.toUpperCase(activeAppName.charAt(0));
-            activeAppName = firstChar + activeAppName.substring(1);
+            String exeName = exePathStr.substring(exePathStr.lastIndexOf("\\") + 1);
 
-            if (activeAppName.endsWith("64") || activeAppName.endsWith("32")) {
-                activeAppName = activeAppName.substring(0, activeAppName.length() - 2);
+            // Update usageData (add 1 second)
+            if (usageData.keySet().contains(exeName)) {
+                int time = Integer.parseInt(usageData.get(exeName)[0]);
+                usageData.put(exeName, new String[]{null, String.valueOf(time + 1)});
+            } else {
+                usageData.put(exeName, new String[]{null, String.valueOf(1)});
             }
-            System.out.println("Not formatted: " + activeAppNameNotFormatted);
-            System.out.println("Formatted: " + activeAppName);
 
+            // Upload dataUsage to database every minute
+            if (secs >= 5) {
+                System.out.println("DATABASE UPDATE\n" + usageData);
+                updateDB.uploadDataToDB(usageData);
+                usageData.clear();
+                secs = 0;
+            }
+
+                // Format exeName
+//                String formatName = exeName.replace(".exe", "").toLowerCase();
+//                char firstChar = Character.toUpperCase(formatName.charAt(0));
+//                formatName = firstChar + formatName.substring(1);
+//
+//                if (formatName.endsWith("64") || formatName.endsWith("32")) {
+//                    formatName = formatName.substring(0, formatName.length() - 2);
         }
     }
 }
+
--- src/main/java/org/example/tline/threads/UpdateDataBase.java
+++ src/main/java/org/example/tline/threads/UpdateDataBase.java
@@ -3,33 +3,43 @@ package org.example.tline.threads;
 import org.sqlite.SQLiteConnection;
 import java.awt.*;
 import java.sql.*;
+import java.util.Arrays;
 import java.util.Set;
 import java.util.TreeMap;
 
 public class UpdateDataBase {
 
     private static final String DB_PATH = "jdbc:sqlite:resources\\databases\\time-data.db";
-    private Statement statement;
-    private final Connection connection;
-    private TreeMap<String, Integer> usageData;
+    private TreeMap<String, String[]> usageData = new TreeMap<>(); // exe_name {usage_time, format_name}
 
     public UpdateDataBase() {
 
-        try (Connection con = DriverManager.getConnection(DB_PATH)) {
-            this.connection = con;
-            statement = connection.createStatement();
+        try (Connection connection = DriverManager.getConnection(DB_PATH)) {
+            Statement statement = connection.createStatement();
             statement.execute(
                     "CREATE TABLE IF NOT EXISTS apps_time " +
-                    "(process_name TEXT, " +
-                    "name_formatted TEXT, " +
-                    "time INTEGER)");
+                    "(exe_name TEXT, " +
+                    "format_name TEXT, " +
+                    "usage_time INTEGER);");
+
+            // Put values in usageData
+            ResultSet data = statement.executeQuery("SELECT * FROM apps_time;");
+            while (data.next()) {
+                usageData.put(data.getString(1),
+                        new String[] {data.getString(2), data.getString(3)});
+            }
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public Set<String> getAppsInDB() {
-        return usageData.keySet();
+    public TreeMap<String, String[]> getUsageData() {
+        return usageData;
+    }
+
+    public void uploadDataToDB(TreeMap<String, String[]> map) {
+
+
     }
 }
